58d50da1c139740685f2d4debe7d4ea3ade68f69
AntonioCS <antoniocs@gmail.com>
2016-05-08 18:53:24 +0100

    Capturing the state of the ball and handling it

--- src/pong.c
+++ src/pong.c
@@ -8,15 +8,19 @@ void Pong_Init(const int window_height, const int window_width) {
 
     p_data->window = init_window(window_height, window_width);
     p_data->ball = init_ball();
+
     p_data->player1 = init_player1();
     p_data->player2 = init_player2();
+
+    p_data->p1_score = 0;
+    p_data->p2_score = 0;
 }
 
 void Pong_Handle(const Uint8 *keys) {
     handle_keys(p_data, keys);
     handle_players(p_data);
-    handle_ball(p_data);
-    
+    enum Ball_State bstate = handle_ball(p_data);
+    handle_scores(p_data, bstate);
 }
 
 void Pong_Draw(SDL_Renderer *gRenderer) {
@@ -26,5 +30,5 @@ void Pong_Draw(SDL_Renderer *gRenderer) {
     SDL_RenderDrawRect(gRenderer, p_data->player2);
 
     //Draw "ball"
-    SDL_RenderDrawRect(gRenderer, &(p_data->ball->coords));    
+    SDL_RenderDrawRect(gRenderer, &(p_data->ball->coords));
 }
