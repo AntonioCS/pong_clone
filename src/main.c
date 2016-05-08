73bc3b6c8e0a82a6ba4eafc86c78936e85b501f1
AntonioCS <antoniocs@gmail.com>
2016-05-08 21:54:55 +0100

    Removed unused code

--- src/main.c
+++ src/main.c
@@ -4,31 +4,14 @@
 #include <stdbool.h>
 #include "pong.h"
 
-#define BLACK 0, 0, 0
-#define WHITE 255, 255, 255
-
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-const Uint8 *key_states;
-
 int main(int argc, char* args[]) {
-    //The window we'll be rendering to
+    //Screen dimension constants
+    const int SCREEN_WIDTH = 640;
+    const int SCREEN_HEIGHT = 480;
+
+    const Uint8 *key_states;
+
     SDL_Window* window = NULL;
-
-    /*
-    SDL_Rect player1 = {.x = 20, .y = 30, .w = 20, .h = 100};
-    SDL_Rect player2 = {.x = 600, .y = 30, .w = 20, .h = 100};
-
-    struct Ball ball = {
-        .coords.x = 120,
-        .coords.y = 30,
-        .coords.w = 20,
-        .coords.h = 20,
-        .left = 0,
-        .up = 0
-    };
-     */
 
     //Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) == 0) {
@@ -67,30 +50,10 @@ int main(int argc, char* args[]) {
             }
 
             if (paused == false) {
-                //Clear screen 
-                SDL_SetRenderDrawColor(gRenderer, WHITE, 0xFF);
-                SDL_RenderClear(gRenderer);
-
                 key_states = SDL_GetKeyboardState(NULL);
 
                 Pong_Handle(key_states);
                 Pong_Draw(gRenderer);
-
-
-                /*
-
-              
-
-                player_movement(&player1);
-                player_movement(&player2);
-
-                //Draw Rect
-                SDL_SetRenderDrawColor(gRenderer, BLACK, 0xFF);
-                SDL_RenderDrawRect(gRenderer, &player1);
-                SDL_RenderDrawRect(gRenderer, &player2);
-
-                move_ball(&ball, &player1, &player2);
-                SDL_RenderDrawRect(gRenderer, &(ball.coords));*/
             }
             //Update Screen
             SDL_RenderPresent(gRenderer);
