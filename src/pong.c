6af359426c95ebc5b702c2af74220823cee721d8
AntonioCS <antoniocs@gmail.com>
2016-05-10 22:35:52 +0100

    Very raw code to show some Test string on the screen. Still needs a lot of work

--- src/pong.c
+++ src/pong.c
@@ -3,6 +3,11 @@
 
 struct Pong_Data *p_data;
 
+static SDL_Color color;
+static TTF_Font *font;
+static SDL_Surface *font_text;
+static SDL_Texture *mTexture;
+
 void Pong_Init(const int window_height, const int window_width) {
     p_data = malloc(sizeof (struct Pong_Data));
 
@@ -14,6 +19,17 @@ void Pong_Init(const int window_height, const int window_width) {
 
     p_data->p1_score = 0;
     p_data->p2_score = 0;
+
+    font = TTF_OpenFont("./resources/font/lazy.ttf", 48);
+    color.r = 0;
+    color.g = 0;
+    color.b = 0;
+
+    if (font == NULL) {
+        printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+    }
+
+    font_text = TTF_RenderText_Solid(font, "Teste", color);
 }
 
 void Pong_Handle(const Uint8 *keys) {
@@ -24,6 +40,14 @@ void Pong_Handle(const Uint8 *keys) {
 }
 
 void Pong_Draw(SDL_Renderer *gRenderer) {
+    SDL_Rect test_struct = {
+        .x = 10,
+        .y = 20,
+        .w = 50,
+        .h = 20
+    };
+    mTexture = SDL_CreateTextureFromSurface( gRenderer, font_text);
+
     //Clear screen
     SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 0xFF);
     SDL_RenderClear(gRenderer);
@@ -35,4 +59,7 @@ void Pong_Draw(SDL_Renderer *gRenderer) {
 
     //Draw "ball"
     SDL_RenderDrawRect(gRenderer, &(p_data->ball->coords));
+
+    //Draw TExt
+    SDL_RenderCopy(gRenderer, mTexture, NULL, &test_struct);
 }
