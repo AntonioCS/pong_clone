6d6cd474fe160d1c6391da01c912db7b3fd08f75
AntonioCS <antoniocs@gmail.com>
2016-07-01 09:53:20 +0100

    Created a define to call the Menu_InitBase with a cast to void*

--- src/Menu.h
+++ src/Menu.h
@@ -12,15 +12,17 @@
 #ifdef __cplusplus
 extern "C" {
 #endif           
-    
+
     typedef struct Menu_Container Menu_Container;
     typedef void (*Menu_ItemAction)(void *);
-    
-    Menu_Container *Menu_Init(SDL_Renderer *, void *[]);
+    Menu_Container *Menu_InitBase(SDL_Renderer *, void *[], void *);
     void Menu_Display(Menu_Container *, int, int);
-    void Menu_Handle(Menu_Container *, const Uint8 );
-    
+    void Menu_Handle(Menu_Container *, const Uint8);
+
     //void Menu_ItemAdd(Menu_Container *, char *, Menu_ItemAction);
+    //@todo Add Menu_Destroy
+
+#define Menu_Init(a,b,c) Menu_InitBase(a, b, (void *)c);
 
 #ifdef __cplusplus
 }
